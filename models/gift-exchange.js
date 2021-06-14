921e230db2003a03f099c890133e99490210b116
Ann <annie.wang50@yahoo.com>
2021-06-14 14:40:47 -0400

    gift-exchange tradiitonal function

--- models/gift-exchange.js
+++ models/gift-exchange.js
@@ -1,9 +1,11 @@
 
 class giftExchange {
+
+  /** This will pass the list of users that implements an algorithm 
+  * responsible for randomly pairing users together.
+  */ 
   static async pairs(names) {
-
     const allParings = []
-
     while (names.length > 0) {
       const paring = []
       var randomPair = Math.floor(Math.random() * names.length)
@@ -14,8 +16,23 @@ class giftExchange {
       names.splice(randomPair, 1)
       allParings.push(paring)
     }
-    
     return allParings
+  }
+
+  static async traditional(names) {
+    const wordsOfPairings = []
+    var randomPair = Math.floor(Math.random() * names.length)
+    const firstUser = names[randomPair]
+
+    while (names.length > 1) {
+      var words = `${names[randomPair]} is giving a gift to `
+      names.splice(randomPair, 1)
+      randomPair = Math.floor(Math.random() * names.length)
+      words += names[randomPair]
+      wordsOfPairings.push(words)
+    }
+    wordsOfPairings.push(`${names[randomPair]} is giving a gift to ${firstUser}`)
+    return wordsOfPairings
   }
 }
 
--- routes/gift-exchange.js
+++ routes/gift-exchange.js
@@ -1,24 +1,26 @@
 const express = require("express")
+const { traditional } = require("../models/gift-exchange")
 const giftExchange = require("../models/gift-exchange")
 const router = express.Router()
-
-const gifts = {
-  pen: 0,
-  pencil: 0,
-}
 
 router.get("/", async(req, res, next) => {
   res.status(200).json(gifts)
 })
 
-router.post("/:giftName", async(req, res, next) => {
+router.post("/pairs/:pairs", async(req, res, next) => {
   const names = req.body.names
-
-  console.log(names)
 
   const pairs = await giftExchange.pairs(names)
 
   res.status(200).json(pairs)
+})
+
+router.post("/traditional/:traditional", async(req, res, next) => {
+  const names = req.body.names
+
+  const traditional = await giftExchange.traditional(names)
+
+  res.status(200).json(traditional)
 })
 
 module.exports = router
